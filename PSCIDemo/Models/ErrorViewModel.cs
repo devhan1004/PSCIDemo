e91e23bdfc492502f4e55933d67769e97ff402c0
devhan1004 <devhan1004@gmail.com>
2025-04-06 20:45:21 -0400

    Add project files.

---
+++ PSCIDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSCIDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
